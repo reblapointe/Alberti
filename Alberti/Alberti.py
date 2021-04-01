860fa34f20bc969502c3ca4053812599072e4e2d
RebeccaLapointe <reblapointe@gmail.com>
2021-04-01 12:21:30 -0400

    type hint for functions

--- Alberti/Alberti.py
+++ Alberti/Alberti.py
@@ -4,19 +4,19 @@ class albertiCypher:
 	movable =     "gklnprtuz&xysomqihfdbace"
 	modulo = len(stationnary)
 
-def encryptChar(cypher:albertiCypher, message:str, key:int): 
+def encryptChar(cypher:albertiCypher, message:str, key:int) -> str: 
 	posMess = cypher.stationnary.find(message)
 	posCypher = (posMess + key) % cypher.modulo
 	return cypher.movable[posCypher]
 
-def encrypt(cypher:albertiCypher, message:str, key:str):
+def encrypt(cypher:albertiCypher, message:str, key:str) -> str: 
 	cryptogram = ""
 	posKey = cypher.stationnary.find(key)
 	for c in message:
 		cryptogram += encryptChar(cypher,c, posKey)
 	return cryptogram
 
-def encode(cypher:albertiCypher, message):
+def encode(cypher:albertiCypher, message) -> str: 
 	return
 
 cryptogram = encrypt(albertiCypher,"VNEGROSSEFACE","D")
