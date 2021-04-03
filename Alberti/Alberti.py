3e75a4d21ce9064e8f8b59a07ef6ba186779338c
RebeccaLapointe <reblapointe@gmail.com>
2021-04-02 20:56:43 -0400

    Avec changement de lettre

--- Alberti/Alberti.py
+++ Alberti/Alberti.py
@@ -1,10 +1,13 @@
 from typing import List
 from typing import Tuple
+import math
+import random
 
-class albertiCypher:
+class Disk:
 	stationnary = "ABCDEFGILMNOPQRSTVXZ1234"
 	movable =     "gklnprtuz&xysomqihfdbace"
-	modulo = len(stationnary)
+	size = len(stationnary)
+	nbKeys = len(stationnary) - 4
 
 encoding = {
 	"H" : "FF",
@@ -15,25 +18,61 @@ encoding = {
 	"Y" : "ZZ"
 }
 
+def lengthCrypto():
+	averageLength:float = 5
+
+	return averageLength * math.exp(-1 * random.random() * averageLength)
+
+
+def nextLettre():
+	return random.randint(0, Disk.nbKeys - 1)
+
 
 def encryptChar(message:str, key:int) -> str: 
-	posMess = albertiCypher.stationnary.find(message)
-	posCypher = (posMess + key) % albertiCypher.modulo
-	return albertiCypher.movable[posCypher]
+	posMess = Disk.stationnary.find(message)
+	posCypher = (posMess + key) % Disk.size
+	return Disk.movable[posCypher]
 
 def encode(message) -> str: 
 	for e in encoding:
 		message = message.replace(e, encoding[e])
 	return message
+
+def printDisk(posKey): 
+	print("État du disque : ")
+	for c in Disk.stationnary:
+		print(c, end = '')
+	s = 0
+	print()
+	while s < Disk.size :
+		print(Disk.movable[posKey], end = '')
+		s += 1
+		posKey = (posKey + 1) % Disk.size
+	print()
+
 def encrypt(message:str, key:str) -> str:
 	message = message.upper()
 	message = encode(message)
+	posKeyRotary:int = Disk.movable.find(key)
 	cryptogram = ""
-	posKey = albertiCypher.stationnary.find(key)
-	for c in message:
-		cryptogram += encryptChar(c, posKey)
+	indice:int = 0;
+	print("Key = " + key, end = '')
+	print ("(" + str(Disk.movable.find(key)) + ")")
+	while indice < len(message):
+		indLettre = nextLettre()
+		print ("IndLettre = " + str(indLettre), end = '')
+		cryptogram += Disk.stationnary[indLettre]
+		print ("(" + Disk.stationnary[indLettre] + ")")
+		posKey = (posKeyRotary - indLettre) % Disk.size
+
+		printDisk(posKey)
+		next = min(indice + lengthCrypto(), len(message))
+		while indice < next :
+			cryptogram += encryptChar(message[indice], posKey)
+			indice = indice + 1;
+			
 	return cryptogram
 
 s = input()
-cryptogram = encrypt(s, "D")
+cryptogram = encrypt(s, "k")
 print(cryptogram)
