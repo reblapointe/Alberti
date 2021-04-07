0048308e1d42ee44eb78bebc604c8c42d1a687f1
rebecca <reblapointe@gmail.com>
2021-04-06 20:14:11 -0400

    run avec paramètre

--- Alberti/alberti.py
+++ Alberti/alberti.py
@@ -1,5 +1,9 @@
+# python3 -c 'import alberti; print(alberti.encrypt("patate","e"))'
+
 import math
 import random
+import re
+import sys
 
 class Param :
 	subMessageLength = 10
@@ -10,7 +14,8 @@ class Disk:
 	stationnary = "ABCDEFGILMNOPQRSTVXZ1234"
 	movable =     "gklnprtuz&xysomqihfdbace"
 	size = len(stationnary)
-	nbKeys = len(stationnary) - 4
+	nbKeys = len(stationnary) - 4 
+	regexMessage = "ABCDEFGILMNOPQRSTVXZ"
 	regexCryptogram = "ABCDEFGILMNOPQRSTVXZgklnprtuz&xysomqihfdbace"
         
 encoding = {
@@ -35,9 +40,10 @@ def encryptChar(message:str, key:int) -> str:
 	posCypher = (posMess + key) % Disk.size
 	return Disk.movable[posCypher]
 
-def encode(message) -> str: 
+def encode(message) -> str:
 	for e in encoding:
 		message = message.replace(e, encoding[e])
+	message = re.sub("[^" + Disk.regexMessage + "]", "", message)
 	return message
 
 
@@ -56,18 +62,16 @@ def printDisk(posKey):
 def encrypt(message:str, key:str) -> str:
 	message = message.upper()
 	message = encode(message)
+	print("message = " + message)
+	print("cle = " + key)
 	posKeyRotary:int = Disk.movable.find(key)
 	cryptogram = ""
 	indexMessage:int = 0;
-	print("Key = " + str(Disk.movable.find(key)) + "(" + key + ")")
 	while indexMessage < len(message):
 		indLettre = nextLettre()
 		cryptogram += Disk.stationnary[indLettre]
 		posKey = (posKeyRotary - indLettre) % Disk.size
 		
-		print (Disk.stationnary[indLettre])
-		printDisk(posKey)
-
 		next = min(indexMessage + lengthCrypto(), len(message))
 		while indexMessage < next :
 			cryptogram += encryptChar(message[indexMessage], posKey)
@@ -99,4 +103,22 @@ def run():
         cryptogram = encrypt(s, "e")
         print(cryptogram)
         print()
+
+if __name__ == "__main__":
+	if (len(sys.argv) > 1) :
+		message = sys.argv[1]
+	else :
+		print ("Entrez le message")
+		message = input()
+	if (len(sys.argv) > 2) :
+		key = sys.argv[2][0]
+		if (key not in Disk.movable):
+			print ("La cle " + key + " est invalide. la cle utilisee sera 'k'")
+			key = "k"
+	else :
+		key = "$"
+		while (key not in Disk.movable) :
+			print ("Entrez la cle")
+			key = input()[0]
+	print (encrypt(message, key))
 
